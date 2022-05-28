92a70e1bfce9fa0eeca16aa4afb9fe4ab5a95fb1
Administrator <kamille606@163.com>
2022-05-28 12:54:31 +0800

    redux

--- src/conponents/Count/index.jsx
+++ src/conponents/Count/index.jsx
@@ -1,6 +1,10 @@
 import React, {Component} from "react";
 import store from "../../redux/store";
-import {createIncrementAction, createDecrementAction} from "../../redux/count_action";
+import {
+    createDecrementAction,
+    createIncrementAction,
+    createIncrementAsyncAction
+} from "../../redux/count_action";
 
 export default class Count extends Component {
 
@@ -15,41 +19,37 @@ export default class Count extends Component {
 
     increment = () => {
         const {value} = this.selectNumber
-        store.dispatch(createIncrementAction(value*1))
+        store.dispatch(createIncrementAction(value * 1))
     }
     decrement = () => {
         const {value} = this.selectNumber
-        store.dispatch(createDecrementAction(value*1))
+        store.dispatch(createDecrementAction(value * 1))
     }
     incrementIfOdd = () => {
         const {value} = this.selectNumber
         const count = store.getState()
         if (count % 2 !== 0) {
-            store.dispatch(createIncrementAction(value*1))
+            store.dispatch(createIncrementAction(value * 1))
         }
     }
     incrementIfAsync = () => {
         const {value} = this.selectNumber
-        setTimeout(() => {
-            store.dispatch(createIncrementAction(value*1))
-        }, 500)
+        store.dispatch(createIncrementAsyncAction(value * 1, 500))
     }
 
     render() {
-        return (
-            <div>
-                <h1>获取react属性:{this.state.carName}</h1>
-                <h1>当前求和为:{store.getState()}</h1>
-                <select ref={c => this.selectNumber = c}>
-                    <option value='1'>1</option>
-                    <option value='2'>2</option>
-                    <option value='3'>3</option>
-                </select>
-                <button onClick={this.increment}>+</button>
-                <button onClick={this.decrement}>-</button>
-                <button onClick={this.incrementIfOdd}>当前求和为奇数再加</button>
-                <button onClick={this.incrementIfAsync}>异步＋</button>
-            </div>
-        )
+        return (<div>
+            <h1>获取react属性:{this.state.carName}</h1>
+            <h1>当前求和为:{store.getState()}</h1>
+            <select ref={c => this.selectNumber = c}>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+            </select>
+            <button onClick={this.increment}>+</button>
+            <button onClick={this.decrement}>-</button>
+            <button onClick={this.incrementIfOdd}>当前求和为奇数再加</button>
+            <button onClick={this.incrementIfAsync}>异步＋</button>
+        </div>)
     }
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import countReducer from './count_reducer'
+import thunk from "redux-thunk";
 
-export default createStore(countReducer);
+export default createStore(countReducer, applyMiddleware(thunk));
