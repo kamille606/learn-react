67360660897fd05c1cb936fe8dedf3790860d649
Administrator <kamille606@163.com>
2022-06-12 22:54:42 +0800

    react hook

--- src/components/5_optimize/index.jsx
+++ src/components/5_optimize/index.jsx
@@ -8,23 +8,35 @@ class Parent extends Component {
         this.setState({car: '迈巴赫'})
     }
 
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return this.state.car !== nextState.car;
+    }
+
     render() {
+        const {car} = this.state
         return (
             <div>
                 <h3>我是父组件</h3>
-                <h4>我的车是{this.state.car}</h4>
+                <h4>我的车是{car}</h4>
                 <button onClick={this.changeCar}>点我换车</button>
-                <Child/>
+                <Child car={car}/>
             </div>
         );
     }
 }
 
 class Child extends Component {
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        console.log(nextState)
+        console.log(nextProps)
+    }
+
     render() {
         return (
             <div>
                 <h3>我是子组件</h3>
+                <h4>我爹的车是{this.props.car}</h4>
             </div>
         );
     }
