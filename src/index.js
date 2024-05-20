170fe18477345daba0c1229f81852fe12d481957
PYT <390176441@qq.com>
2024-05-20 20:28:01 +0800

    Changes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {Provider} from 'react-redux'
 import App from './App'
 
 ReactDOM.render(
-    /* Provider使所有子组件都能接收到store123 */
+    /* Provider使所有子组件都能接收到store */
     <Provider store={store}>
         <App/>
     </Provider>, document.getElementById('root')
